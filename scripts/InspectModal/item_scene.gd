be4381cb3459d9e6d2494164ceb2d9bbff7f420d
Suphanath K <suphanath.k@tuta.io>
2025-11-09 13:46:43 +0700

    add rand_point func

--- scripts/InspectModal/item_scene.gd
+++ scripts/InspectModal/item_scene.gd
@@ -9,9 +9,16 @@ extends Node3D
 @export_range(1, 25) var rotation_const: float
 @export var rotate_speed: float = 1.0
 
+var is_label_placed := false
+
 func _process(delta: float) -> void:
 	__do_rotate_obj(delta)
 
+
+func _physics_process(_delta: float) -> void:
+	if not is_label_placed:
+		get_item_surface_point()
+		is_label_placed = true
 
 func __do_rotate_obj(delta: float) -> void:
 	var current = item_holder.rotation_degrees
@@ -37,3 +44,19 @@ func add_rotation(to_add: Vector3) -> void:
 func reset_rotation() -> void:
 	item_rot_target.rotation = Vector3.ZERO
 	item_holder.rotation = Vector3.ZERO
+
+
+# call in physics_process only
+func get_item_surface_point() -> Array[Vector3]:
+	var sphere_point := RandUtil.rand_point_on_sphere(10)
+	var ray_param := PhysicsRayQueryParameters3D.create(sphere_point, Vector3.ZERO)
+	ray_param.collide_with_bodies = true
+	var space_state := get_world_3d().direct_space_state
+	var result := space_state.intersect_ray(ray_param)
+	var item_pos := Vector3.ZERO
+	var item_normal := Vector3.UP
+	if result:
+		item_pos = result.get("position")
+		item_normal = result.get("normal")
+	return [item_pos, item_normal]
+	
