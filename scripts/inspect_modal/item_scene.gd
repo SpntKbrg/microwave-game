7f1948839f762ba2a4adb5be9ef7a52791a80dcd
Suphanath K <suphanath.k@tuta.io>
2025-11-16 17:56:24 +0700

    Add label sticker on object

--- scripts/inspect_modal/item_scene.gd
+++ scripts/inspect_modal/item_scene.gd
@@ -4,6 +4,8 @@ extends Node3D
 @export_group("Internal")
 @export var item_holder: Node3D
 @export var item_rot_target: Node3D
+@export var sticker: Sprite3D
+@export var sticker_texture: Sticker
 
 @export_group("Adjustables")
 @export_range(1, 25) var rotation_const: float
@@ -17,7 +19,10 @@ func _process(delta: float) -> void:
 
 func _physics_process(_delta: float) -> void:
 	if not is_label_placed:
-		get_item_surface_point()
+		var sticker_placement := get_item_surface_point()
+		print(sticker_placement)
+		sticker.position = sticker_placement[0] - (sticker_placement[1] * 0.001)
+		sticker.look_at(sticker_placement[0] + sticker_placement[1])
 		is_label_placed = true
 
 func __do_rotate_obj(delta: float) -> void:
@@ -27,7 +32,6 @@ func __do_rotate_obj(delta: float) -> void:
 		return
 	# rotate item towards target
 	item_holder.rotation_degrees = LerpUtil.exp_decay_vector3(current, target, rotation_const, delta)
-	print(item_holder.rotation_degrees)
 
 
 func __do_set_item() -> void:
@@ -40,23 +44,31 @@ func __do_set_item() -> void:
 func add_rotation(to_add: Vector3) -> void:
 	item_rot_target.rotation_degrees += Vector3(to_add.y, to_add.x, 0) * rotate_speed
 
+func set_target_rotation(to_set: Vector3) -> void:
+	item_rot_target.rotation_degrees = to_set
 
 func reset_rotation() -> void:
 	item_rot_target.rotation = Vector3.ZERO
 	item_holder.rotation = Vector3.ZERO
 
+func get_target_rotation() -> Vector3:
+	return item_rot_target.rotation_degrees
+
+func set_sticker_text(text: String) -> void:
+	sticker_texture.set_label(text)
 
 # call in physics_process only
 func get_item_surface_point() -> Array[Vector3]:
 	var sphere_point := RandUtil.rand_point_on_sphere(10)
+	print({"sphere_point": sphere_point})
 	var ray_param := PhysicsRayQueryParameters3D.create(sphere_point, Vector3.ZERO)
 	ray_param.collide_with_bodies = true
 	var space_state := get_world_3d().direct_space_state
 	var result := space_state.intersect_ray(ray_param)
 	var item_pos := Vector3.ZERO
 	var item_normal := Vector3.UP
+	print(result)
 	if result:
 		item_pos = result.get("position")
-		item_normal = result.get("normal")
+		item_normal = -result.get("normal")
 	return [item_pos, item_normal]
-	
--- scripts/utils/lerp_util.gd
+++ scripts/utils/lerp_util.gd
@@ -6,5 +6,4 @@ extends Node
 # decay_const = rate to transition a to b (range from 1(slow) to 25(fast))
 
 static func exp_decay_vector3(a: Vector3, b: Vector3, decay_const: float, delta: float) -> Vector3:
-	print(exp(-decay_const * delta))
 	return b + (a - b) * exp(-decay_const * delta)
