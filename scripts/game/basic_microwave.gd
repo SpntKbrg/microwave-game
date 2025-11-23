bc93ad68b20ed67edc021009b92a90c434c63461
cahharmip <sorapol57@gmail.com>
2025-11-23 16:24:58 +0900

    add: game scene and try create microwave

--- scripts/game/basic_microwave.gd
+++ scripts/game/basic_microwave.gd
@@ -2,11 +2,15 @@ class_name BasicMicrowave extends IMicrowave
 
 var item: ItemModel
 
+func _ready() -> void:
+	setup()
+
 func setup()->void:
-	pass
+	print("basic microwave seting up..")
+	microwave_ui.visible = false
 
 func on_item_drop_addition(_item: ItemModel) -> void:
 	item = _item
 
-func on_commit_command(_command: MicrowaveMethod) -> void:
+func on_commit_command(_command: MicrowaveMethod, _time: int) -> void:
 	item.tick(_command)
