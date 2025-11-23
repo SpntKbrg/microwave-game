cb066c1649347d8753c62bec71dc182301ad9d9a
cahharmip <sorapol57@gmail.com>
2025-11-23 16:24:58 +0900

    add: game scene and try create microwave

--- scripts/game/microwave_factory.gd
+++ scripts/game/microwave_factory.gd
@@ -1,8 +1,11 @@
 class_name MicrowaveFactory extends Node
 
+const basic_microwave = preload("res://scenes/mockup/basic_microwave.tscn")
+
 func _init() -> void:
 	pass
 
-func create(spec: MicrowaveSpec) -> BasicMicrowave:
+func create(spec: MicrowaveSpec) -> Node2D:
 	#check spec then gen microwave
-	return BasicMicrowave.new()
+	print("spec", spec.useTemperature)
+	return basic_microwave.instantiate();
