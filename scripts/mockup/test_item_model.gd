4446989b6e53868f2a134f3f6036b306a74f9317
RocKIn0X <dantai39@gmail.com>
2025-11-28 00:31:17 +0700

    rename autoload to ItemResource

--- scripts/mockup/test_item_model.gd
+++ scripts/mockup/test_item_model.gd
@@ -7,7 +7,7 @@ extends Node2D
 @onready var reset_button: Button = $CanvasLayer/Control/HBoxContainer/Reset;
 
 func _ready() -> void:
-	var model_data = item_model_resource.getModel(UtilType.ItemType.STEAK);
+	var model_data = ItemResource.getModel(UtilType.ItemType.STEAK);
 	var item_data = ItemData.new(UtilType.ItemType.STEAK, 5, UtilType.WaveTemperature.MEDIUM, model_data.raw_model, model_data.cooked_model);
 	item.set_data(item_data);
 	var screen_size = get_viewport().size
