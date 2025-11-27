516b8532cc1d29608b0f163e59a9e3dfb11351e9
RocKIn0X <dantai39@gmail.com>
2025-11-28 00:42:02 +0700

    remove autoload and add model resource as child of test item model scene

--- scripts/mockup/test_item_model.gd
+++ scripts/mockup/test_item_model.gd
@@ -5,9 +5,10 @@ extends Node2D
 @onready var medium_button: Button = $CanvasLayer/Control/HBoxContainer/medium_tick;
 @onready var high_button: Button = $CanvasLayer/Control/HBoxContainer/high_tick;
 @onready var reset_button: Button = $CanvasLayer/Control/HBoxContainer/Reset;
+@onready var model_resource: ModelResource = $ModelResource;
 
 func _ready() -> void:
-	var model_data = ItemResource.getModel(UtilType.ItemType.STEAK);
+	var model_data = model_resource.getModel(UtilType.ItemType.STEAK);
 	var item_data = ItemData.new(UtilType.ItemType.STEAK, 5, UtilType.WaveTemperature.MEDIUM, model_data.raw_model, model_data.cooked_model);
 	item.set_data(item_data);
 	var screen_size = get_viewport().size
--- scripts/resources/model_resource.gd
+++ scripts/resources/model_resource.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+class_name ModelResource
+
 @export var data: Dictionary[UtilType.ItemType, ModelData];
 
 func getModel(type: UtilType.ItemType):
