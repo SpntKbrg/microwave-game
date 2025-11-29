4ec10925c12b3b335711f0e2318c930a5dc25087
Sorapol <sorapol57@gmail.com>
2025-11-29 23:50:51 +0900

    Microwave unit with item model (#14)
    * Add: make microwave open ui properly
    
    * Add: make microwave now can tick item

--- scripts/game/basic_microwave.gd
+++ scripts/game/basic_microwave.gd
@@ -2,16 +2,18 @@ class_name BasicMicrowave extends IMicrowave
 
 var item: ItemModel
 
-func _ready() -> void:
-	super._ready()
-	setup()
-
 func setup()->void:
 	print("basic microwave seting up..")
-	microwave_ui.visible = false
+	
+func _ready() -> void:
+	super._ready()
 
 func on_item_drop_addition(_item: ItemModel) -> void:
+	if not microwave_ui.is_connected("on_commit_command", on_commit_command):
+		print("connect on_commit_command")
+		microwave_ui.on_commit_command.connect(on_commit_command)
 	item = _item
 
 func on_commit_command(_command: MicrowaveMethod, _time: int) -> void:
+	print("do on_commit_command")
 	item.tick(_command)
