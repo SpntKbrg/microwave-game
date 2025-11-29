25491c00f7d6c0b611d6872f89cab24cabf130d5
cahharmip <sorapol57@gmail.com>
2025-11-30 01:37:17 +0900

    Fixed: microwave ui and microwave will block when running and show progress

--- scripts/game/basic_microwave.gd
+++ scripts/game/basic_microwave.gd
@@ -1,12 +1,25 @@
 class_name BasicMicrowave extends IMicrowave
 
+@export var microwave_timer: Timer
+@export var microwave_progress: ProgressBar
 var item: ItemModel
+var countdown_ms: int
+var sec_modifier: int = 60 # 1 = real time, 60 = 1 real sec = 1 game min
 
 func setup()->void:
 	print("basic microwave seting up..")
+	microwave_timer.timeout.connect(on_countdown)
 	
 func _ready() -> void:
 	super._ready()
+
+func _process(_delta: float) -> void:
+	microwave_progress.visible = is_running
+	if not is_running:
+		return
+	var countdown_sec := countdown_ms / 1000.0
+	var percent = (countdown_sec -  microwave_timer.time_left) / countdown_sec
+	microwave_progress.value = percent * 100.0
 
 func on_item_drop_addition(_item: ItemModel) -> void:
 	if not microwave_ui.is_connected("on_commit_command", on_commit_command):
@@ -14,6 +27,14 @@ func on_item_drop_addition(_item: ItemModel) -> void:
 		microwave_ui.on_commit_command.connect(on_commit_command)
 	item = _item
 
+func on_countdown()->void:
+	is_running = false
+
 func on_commit_command(_command: MicrowaveMethod, _time: int) -> void:
 	print("do on_commit_command")
+	is_running = true
+	var input_min_in_sec := floori(_time / 100.0) * 60
+	var input_sec := _time % 100
+	countdown_ms = floori((input_min_in_sec + input_sec) * 1000.0 / sec_modifier) # realtime til finished
+	microwave_timer.start(countdown_ms / 1000.0)
 	item.tick(_command)
